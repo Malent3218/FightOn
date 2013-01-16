3da5f25733754dabe436648055fedfd7cc2acb8b
Timothy So <sot@usc.edu>
2013-01-15 18:47:44 -0800

    Created the FightOn HelloWorld-like application

---
+++ FightOn.cpp
@@ -0,0 +1,17 @@
+/*
+ *	FightOn.cpp
+ *	
+ *	Written by: Timothy So
+ *	Date: 1/15/12
+ *	
+ *	HelloWorld-style program that prints "Fight On!"
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Fight On!\n";
+	return 0;
+}
